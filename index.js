5aafa2a671123aab77a9e36a16d94fabdbc691bd
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-11-21 20:18:44 +0100

    Add charset replace

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-'use strict';
+'use strict'
 
-var jschardet = require('jschardet');
-var charset = require('charset');
-var iconv = require('iconv-lite');
+var jschardet = require('jschardet')
+var iconv = require('iconv-lite')
+var charset = require('charset')
 
-module.exports = ensureUTF8;
+var charsetRegex = /charset=["]*([^>"\s]+)/i
 
-function ensureUTF8(binaryBuffer, contentType) {
-  var encoding = detectEncoding(binaryBuffer, contentType);
-
-  if (encoding === 'utf8') return binaryBuffer.toString('utf8');
-
-  return iconv.decode(binaryBuffer, encoding);
+module.exports = function ensureUTF8 (binaryBuffer, contentType) {
+  var encoding = getEncoding(binaryBuffer, contentType)
+  
+  return encoding === 'utf8'
+    ? binaryBuffer.toString('utf8')
+    : iconv.decode(binaryBuffer, encoding).replace(charsetRegex, 'utf-8')
 }
 
-function detectEncoding(content, contentType) {
-  var byMetaAndContentType = charset({'content-type': contentType}, content);
-  if (byMetaAndContentType) return byMetaAndContentType;
+function getEncoding (content, contentType) {
+  return charset({'content-type': contentType}, content) || inferredEncoding(content) || 'utf8'
+}
 
-  var byGuessing = jschardet.detect(content);
-  if (byGuessing && byGuessing.encoding) return byGuessing.encoding;
-
-  return 'utf8';
+function inferredEncoding(content) {
+  var charset = jschardet.detect(content);
+  return charset && charset.encoding && charset.encoding
 }
