0e6783006db1bf21cc73d8f00ce46d5a3afec511
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-01-16 22:51:57 +0100

    build: automate release

--- test/index.js
+++ test/index.js
@@ -1,41 +1,33 @@
 'use strict'
 
-const { expect } = require('chai')
+const test = require('ava')
 const path = require('path')
 const fs = require('fs')
 
+const fixture = name => fs.readFileSync(path.join(__dirname, '/fixtures/', name))
+
 const toUTF8 = require('..')('utf-8')
 
-describe('encoding conversion', function () {
-  it('Shift-JIS', function () {
-    const buffer = loadExample('51242_54045.html')
-    const output = toUTF8(buffer, 'text/html')
-
-    expect(output).to.contain('或る日の小せん')
-  })
-
-  it('Windows-1250', function () {
-    const buffer = loadExample('rp.pl.html')
-    const output = toUTF8(buffer, 'windows-1250')
-
-    expect(output).to.contain('majątków')
-  })
-
-  it('UTF-8', function () {
-    const buffer = loadExample('utf8.with.meta.html')
-    const output = toUTF8(buffer, 'text/html')
-
-    expect(output).to.contain('日本語')
-  })
-
-  it('inferred', function () {
-    const buffer = loadExample('shiftjis.no.meta.html')
-    const output = toUTF8(buffer, 'text/html')
-
-    expect(output).to.contain('次常用國字標準字體表')
-  })
+test('Shift-JIS', t => {
+  const buffer = fixture('51242_54045.html')
+  const output = toUTF8(buffer, 'text/html')
+  t.true(output.includes('或る日の小せん'))
 })
 
-function loadExample (name) {
-  return fs.readFileSync(path.join(__dirname, '/fixtures/', name))
-}
+test('Windows-1250', t => {
+  const buffer = fixture('rp.pl.html')
+  const output = toUTF8(buffer, 'windows-1250')
+  t.true(output.includes('majątków'))
+})
+
+test('UTF-8', t => {
+  const buffer = fixture('utf8.with.meta.html')
+  const output = toUTF8(buffer, 'text/html')
+  t.true(output.includes('日本語'))
+})
+
+test('inferred', t => {
+  const buffer = fixture('shiftjis.no.meta.html')
+  const output = toUTF8(buffer, 'text/html')
+  t.true(output.includes('次常用國字標準字體表'))
+})
